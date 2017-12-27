9d37516700845c7a26f259bc1ea42ba98e646f54
Josh <joshsurette@Joshs-MacBook-Air.local>
2017-12-27 17:55:02 -0500

    Adding in dummy health check

---
+++ innovateEDU-app/src/main/java/com/innovateEDU/app/health/HelloResourceTest.java
@@ -0,0 +1,14 @@
+package com.innovateEDU.app.health;
+
+import com.codahale.metrics.health.HealthCheck;
+
+public class HelloResourceTest extends HealthCheck {
+
+    @Override
+    protected Result check() throws Exception {
+
+        return Result.healthy();
+
+    }
+
+}
--- innovateEDU-app/src/main/java/com/innovateEDU/app/innovateEDUApplication.java
+++ innovateEDU-app/src/main/java/com/innovateEDU/app/innovateEDUApplication.java
@@ -5,6 +5,7 @@ import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 import com.innovateEDU.app.resources.HelloResource;
+import com.innovateEDU.app.health.HelloResourceTest;
 
 public class innovateEDUApplication extends Application<innovateEDUConfiguration> {
 
@@ -17,6 +18,12 @@ public class innovateEDUApplication extends Application<innovateEDUConfiguration
     @Override
     public void run(innovateEDUConfiguration configuration,
                     Environment environment) {
+
+        // Run Health Checks
+        final HelloResourceTest healthCheck = new HelloResourceTest();
+
+        // Register Health Checks in environment
+        environment.healthChecks().register("hello", healthCheck);
 
         // Register Jersey resources in Environment
         environment.jersey().register(new HelloResource());
