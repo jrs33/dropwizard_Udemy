efb2c5258fd63bb67086292c2be72ca4241a027b
Josh <surettej@bu.edu>
2017-12-30 18:41:51 -0500

    Adding in MySQL connection

--- innovateLocker/src/main/java/com/innovateEDU/innovateLockerConfiguration.java
+++ innovateLocker/src/main/java/com/innovateEDU/innovateLockerConfiguration.java
@@ -2,13 +2,27 @@ package com.innovateEDU;
 
 import io.dropwizard.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.db.DataSourceFactory;
 import org.hibernate.validator.constraints.*;
+
+import javax.validation.Valid;
 import javax.validation.constraints.*;
+import javax.xml.crypto.Data;
 
 public class innovateLockerConfiguration extends Configuration {
 
     @NotEmpty
     private String password;
+
+    @NotNull
+    @Valid
+    private DataSourceFactory dataSourceFactory
+            = new DataSourceFactory();
+
+    @JsonProperty("database") // pass database keyword from yml
+    public DataSourceFactory getDataSourceFactory() {
+        return dataSourceFactory;
+    }
 
     @JsonProperty
     public String getPassword() {
