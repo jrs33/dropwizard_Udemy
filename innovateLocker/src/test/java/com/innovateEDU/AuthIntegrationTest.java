815224bc5604d01f839e41edde1663b46345b21f
Josh <surettej@bu.edu>
2017-12-30 15:07:29 -0500

    finalized https authorization unit test

--- innovateLocker/src/test/java/com/innovateEDU/AuthIntegrationTest.java
+++ innovateLocker/src/test/java/com/innovateEDU/AuthIntegrationTest.java
@@ -2,11 +2,14 @@ package com.innovateEDU;
 
 import io.dropwizard.testing.junit.DropwizardAppRule;
 import io.dropwizard.testing.junit.ResourceTestRule;
+import org.glassfish.jersey.SslConfigurator;
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 
+import javax.net.ssl.SSLContext;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
@@ -24,17 +27,31 @@ public class AuthIntegrationTest {
             = new DropwizardAppRule<>(innovateLockerApplication.class, CONFIG_PATH);
 
     private static final String TARGET
-            = "http://localhost:8080";
+            = "https://localhost:8443";
 
     private static final String PATH
             = "hello/secured";
 
+    private static final String TRUST_STORE_FILE_NAME
+            = "innovateEDU.keystore";
+
+    private static final String TRUST_STORE_PASSWORD
+            = "p@ssw0rd";
+
+    private HttpAuthenticationFeature FEATURE
+            = HttpAuthenticationFeature.basic("username", "p@ssw0rd");
+
     private Client client;
 
-    // Run before every single test
+    // Run before every single test and creates a secure https connection
     @Before
     public void setUp() {
-        client = ClientBuilder.newClient();
+        SslConfigurator configurator
+                = SslConfigurator.newInstance();
+        configurator.trustStoreFile(TRUST_STORE_FILE_NAME)
+                .trustStorePassword(TRUST_STORE_PASSWORD);
+        SSLContext context = configurator.createSSLContext();
+        client = ClientBuilder.newBuilder().sslContext(context).build();
     }
 
     // Run after each test method
@@ -51,6 +68,18 @@ public class AuthIntegrationTest {
                 .request()
                 .get();
         assertEquals(Response.Status.UNAUTHORIZED.getStatusCode(), response.getStatus());
+    }
+
+    @Test
+    public void testGoodPath() {
+        String expected = "Hello secured world";
+        client.register(FEATURE);
+        String actual = client
+                .target(TARGET)
+                .path(PATH)
+                .request(MediaType.TEXT_PLAIN)
+                .get(String.class);
+        assertEquals(expected, actual);
     }
 
 }
